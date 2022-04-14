501324f0cb06143a8f0b36f312efcce042010d86
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-04-14 09:03:59 +0300

    Update 09_dates.php

--- php course/09_dates.php
+++ php course/09_dates.php
@@ -1,13 +1,19 @@
 <?php
 
 // Print current date
-
+echo 'Today is: '. date('y - m - d h:i:s') . '<br>';
+echo 'Today is: '. date('Y - M - D H:i:s') . '<br>';
+echo 'Today is: '. date('D - M - Y ') . 'and the time is '. date('H : i : s') . '<br>';
 // Print yesterday
+echo 'Yesterday was: ' . date('y m d H:i:s', time()  - 60 / 60-24) . '<br>';
 
-// Different format: https://www.php.net/manual/en/function.date.php
 
 // Print current timestamp
+echo time() /60/60/24/30/12 . '<br>';
 
 // Parse date: https://www.php.net/manual/en/function.date-parse.php
+
+// $parseDate = date_parse('2022-04-13');
+// print_r($parseDate);
 
 // Parse date from format: https://www.php.net/manual/en/function.date-parse-from-format.php
