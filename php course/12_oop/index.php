6c4468f7f86570e33224eca1969a52b112ca4867
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-04-16 08:35:07 +0300

    Update index.php

--- php course/12_oop/index.php
+++ php course/12_oop/index.php
@@ -1,9 +1,23 @@
 <?php
 
 // What is class and instance
+// class Person{
+//     public $name;
+//     public $surname;
+//     public $age;
 
-// Create Person class in Person.php
+    
+// }
 
-// Create instance of Person
+// $person = new Person();
+// $person->name = 'Alex';
+// $person->age =22;
+// $person->surname = 'Gabriel';
 
-// Using setter and getter
+// echo '<pre>';
+// var_dump($person);
+// echo '<pre>';
+
+// echo $person->name . '<br>';
+// echo $person->age . '<br>';
+// echo $person->surname . '<br>';
