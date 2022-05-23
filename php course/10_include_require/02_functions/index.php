da866e8df5eef1aaefa3142c85ad36e4488ac2b7
alex100588 <49831245+alex100588@users.noreply.github.com>
2022-05-23 12:37:26 +0300

    Update index.php

--- php course/10_include_require/02_functions/index.php
+++ php course/10_include_require/02_functions/index.php
@@ -9,3 +9,13 @@ echo add(25, 12);
 echo substract(22, 14);
 echo substract(32, 23);
 echo substract(65, 12);
+
+echo add(2,3);
+echo add(44, 12);
+echo add(12, 23);
+echo add(11,43);
+
+echo subtract(2,3);
+echo subtract(65,10);
+echo subtract(11, 43);
+echo subtract(32,12);
